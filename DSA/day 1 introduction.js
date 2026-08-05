7a7a25b615f22ac6128eb980b7432a9f6e000ecf
naphees <naphees9997@gmail.com>
2026-08-05 12:39:41 +0530

    Day 1 DSA Overview

---
+++ DSA/day 1 introduction.js
@@ -0,0 +1,25 @@
+// Date : 5-8-2026
+// Topic : What is DSA ?
+
+// DSA stands for:
+// D = Data
+// S = Structure
+// A = Algorithms
+
+// in simple words: 
+// DSA  is the study to organize data effiently and
+// solve problems using step-by-step methods.
+
+// Examples : 
+// Data Structure           Real-life Example
+// Array                    A row of lockers
+// Stack                    A stack of plates
+// Queue                    People waiting in a ticket  line
+// Linked List              Train coaches connected together
+// Tree                     Family tree or file system
+// Graph                    Google Maps or social networks
+// Hash Map                 Dictionary (word -> meaning)
+
+
+// Algorithm
+//An Algorithm is a step-by-step process to solve a problem.
